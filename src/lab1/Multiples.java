8814e83b17655d028ffff5ea66055a3a65b83af9
Nabiha Tariq <nabiha.t101@gmail.com>
2026-09-15 14:10:23 -0400

    adding new methods

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,16 @@
+package lab1;
+
+
+class Multiples {
+    public static void main() {
+        number = 0;
+        for (i=1, i<=1000, i++) {
+            if (i%3 == 0) {
+                number += 1;
+            } else if (i%5 == 0) {
+                number += 1;
+            }
+        }
+    }
+}
+
